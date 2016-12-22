f8f77696a3a440c6a657a1b4603da22163a4a848
Paul Clements <paulcclements@gmail.com>
2016-12-22 14:26:01 -0600

    Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿$env:path += ";C:\Program Files (x86)\vim\vim74"
+$env:path += ";C:\Program Files\git\bin;C:\Program Files\Git\usr\bin"
+
+# Load posh-git example profile
+. 'D:\Users\Paul\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
